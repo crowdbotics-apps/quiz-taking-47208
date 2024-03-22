b694245ee7c2ad108e645c00147b46df42cce1f2
Crowdbotics <team@crowdbotics.com>
2024-03-22 07:42:16 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "quiz-taking-47208",
+  url: "https://quiz-taking-47208.botics.co",
+  api: "https://quiz-taking-47208.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
